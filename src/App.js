c2992e4c424b77b68496846d658f3cc53e7c8107
David Benjamin <davidnbenjamin15@gmail.com>
2024-08-17 23:57:58 -0400

    Add Echelon branding.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import NavBar from './components/NavBar';
 import ModelViewer from './components/ModelViewer';
 import BottomTab from './components/BottomTab';
+import PoweredByEch from './components/PoweredByEch';
 
 function App() {
   const [model, setModel] = useState('chair1.glb');
@@ -11,6 +12,7 @@ function App() {
     <>
       <div className='inline-flex flex-col align-middle justify-center w-full'>
         <NavBar />
+        <PoweredByEch />
       </div>
       <div className="flex justify-center bg-slate-800 h-screen">
         <ModelViewer modelSource={`models/${model}`} />
---
+++ src/components/PoweredByEch.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const PoweredByEch = () => {
+  return (
+    <div className='bg-slate-800 mt-20 flex p-5 w-full justify-center'>
+      <p className='font-medium text-xl text-slate-1000'>AR experience powered by</p>
+      <img src='./echLogo.png' alt='Echelon logo' className='size-9'/>
+    </div>
+  )
+}
+
+export default PoweredByEch
